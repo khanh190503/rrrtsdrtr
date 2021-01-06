acbaf61aa1a778c8217b6c8a65a2d742bd86bf6e
khanh190503 <77036479+khanh190503@users.noreply.github.com>
2021-01-06 10:30:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gioi thieu ban than CV</title>
+</head>
+<body style="background-image: url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6f131d22-7974-4793-9db3-2160376b5c72/de1yw9w-905ef3c0-5fd6-4aef-9f07-d6e8417fad2c.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNmYxMzFkMjItNzk3NC00NzkzLTlkYjMtMjE2MDM3NmI1YzcyXC9kZTF5dzl3LTkwNWVmM2MwLTVmZDYtNGFlZi05ZjA3LWQ2ZTg0MTdmYWQyYy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.xp-aVrTIJysQDLwQYM9OKW43yg25f7SLLtsATQ2lu7M);background-size: cover;"><a href="https://www.facebook.com/"></a>
+	<h1> Giới thiệu bản thân</h1><font color="aqua">
+    <h2>khanh190503@gmail.com</h2>
+    <h3>Ha Noi</h3>
+    <a href="https://gokisoft.com/"><img src="https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/109922462_593549701554838_8560804672954938270_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=KUxehF2ESO0AX-Yupy-&_nc_ht=scontent.fhan2-1.fna&oh=7d6436f483ddae338194f43019356399&oe=6019BE18" width="700px"></a>
+    <h2>Nguyen Duy Khanh</h2>
+    <h3>18</h3>
+</body>
+</html>
